5ce98ca66c7d560a73ce6ad21415b2b11e539ea3
luojinjiang <1534730132@qq.com>
2020-03-14 23:48:38 +0800

    提交代码第一版

---
+++ src/main/java/cn/objectspace/webssh/controller/RouterController.java
@@ -0,0 +1,17 @@
+package cn.objectspace.webssh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RouterController {
+    @RequestMapping("/websshpage")
+    public String websshpage(){
+        return "webssh";
+    }
+
+    @RequestMapping("/login")
+    public String client(){
+        return "login";
+    }
+}
